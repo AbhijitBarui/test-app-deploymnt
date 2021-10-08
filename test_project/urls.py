8c2b06e095362d25b0bad14f439a68dcc98252b2
Abhijit Barui <abhijitongit@gmail.com>
2021-10-08 15:49:41 +0530

    media in urls

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('test_app.urls')),
     path('listings', include('listings.urls')),
-]
+] + static.static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
