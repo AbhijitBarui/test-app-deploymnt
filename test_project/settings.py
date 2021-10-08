d2a137165b4a890ca5f3b42c93810535608d7a09
Abhijit Barui <abhijitongit@gmail.com>
2021-10-08 09:54:58 +0530

    settings allowed host .com

--- test_project/settings.py
+++ test_project/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['test-app-deploymnt.herokuapp', '127.0.0.1']
+ALLOWED_HOSTS = ['test-app-deploymnt.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
