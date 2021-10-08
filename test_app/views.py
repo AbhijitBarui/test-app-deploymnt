39c84087827e37afbd058a69bd8a482922d13312
Abhijit Barui <abhijitongit@gmail.com>
2021-10-08 12:43:51 +0530

    about urls

--- test_app/views.py
+++ test_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'test_app/index.html')
+
+def about(request):
+    return render(request, 'test_app/about.html')
